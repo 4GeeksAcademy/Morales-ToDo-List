edbd31c4aae0fd636040418d4f67a48ae0a25228
Curs279 <136276439+Curs279@users.noreply.github.com>
2023-11-08 04:00:38 +0000

    Se creo el todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [resultado,setResultado] = useState (""); 
+	const [tareas,setTareas]= useState([]);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid d-flex flex-column align-items-center justify-content-start text-light">
+			<h1>¿Qué tienes que hacer?</h1>
+			<ul>
+				<input class="form-control" type="text" placeholder="Escribe aquí tus tareas :D"
+				onChange={(cambio) => setResultado(cambio.target.value)}
+				value={resultado}
+				onKeyPress={(cambio) => {
+
+					if (cambio.key == "Enter") {
+						setTareas(tareas.concat(resultado));
+					}
+				}
+				}
+				/>
+				{tareas.map((nuevo, lista) => (
+					<li>
+						{nuevo} {""} <i class="fas fa-minus-circle" 
+						onClick={() =>
+							setTareas(
+								tareas.filter(
+									(e, currentIndex) => 
+									lista != currentIndex
+								)
+							)
+						}>
+						</i>
+					</li>
+				))}
+			</ul>
+			<div>{tareas.length} cosas por acabar</div>
 		</div>
 	);
 };
